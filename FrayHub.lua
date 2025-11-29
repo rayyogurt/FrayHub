d6896952d19b2548d6e335af4ef99d99a4e0d136
Fray <raynardmarvellin123@gmail.com>
2025-11-29 15:38:52 +0700

    Update FrayHub.lua

--- FrayHub.lua
+++ FrayHub.lua
@@ -136,7 +136,7 @@ local function SendToDiscord(msg, rarity, colorDec, source, fishData)
 end
 
 -- [[ 3. LOAD UI RAYFIELD ]] --
-local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/rayyogurt/FrayHub/refs/heads/main/UI%20FrayHub"))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
 local Window = Rayfield:CreateWindow({
     Name = "FrayHub | Main Version 1.2",
@@ -747,4 +747,5 @@ MiscTab:CreateToggle({
 })
 
 Rayfield:Notify({Title = "Frayhub Loaded", Content = "All Systems Go!", Duration = 5, Image = nil})
+
 
